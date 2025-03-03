2809d22298ef1e2f85bac991e325d3eff8e21937
Long Tran <ltran@atomiton.com>
2025-03-03 11:35:50 +0700

    update frontend source

--- frontend/src/utils/useClientLayoutEffect.ts
+++
@@ -1,6 +0,0 @@
-import { useLayoutEffect, useEffect } from 'react';
-
-// Use useLayoutEffect on client, useEffect on server
-export const useClientLayoutEffect = typeof window !== 'undefined' 
-  ? useLayoutEffect 
-  : useEffect;
