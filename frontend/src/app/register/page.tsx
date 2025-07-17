6ab0366229e6a0a0422c996714b128a92cfeeb00
Long Tran <ltran@atomiton.com>
2025-07-17 19:24:02 +0700

    Checkpoint

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { useState } from 'react';
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "../../components/ui/button"
+import { Input } from "../../components/ui/input"
+import { Label } from "../../components/ui/label"
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle
-} from "@/components/ui/card"
+} from "../../components/ui/card"
 import { useRouter } from "next/navigation";
 import { API_ENDPOINTS } from "../../lib/api";
 
