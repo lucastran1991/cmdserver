116751ae8ff76c16e0a1965004b1e8ce406bac96
Long Tran <ltran@atomiton.com>
2025-07-20 22:30:20 +0700

    checkpoint

--- server_start.sh
+++ server_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 git pull
 
+pm2 delete all
+
 cd frontend
-pm2 stop nextjs-app
 npm run build
-pm2 start npm --name "nextjs-app" -- start
+pm2 start npm --name "nextjs-frontend" -- start
 cd ..
 
 cd backend
-pm2 stop fastapi-backend
-pm2 start "uvicorn app.app:app --host 0.0.0.0 --port 8000" --interpreter=python3 --name fastapi-backend
+pm2 start "uvicorn app.app:app --host 0.0.0.0 --port 8000 &" --interpreter=python3 --name fastapi-backend
 cd ..
